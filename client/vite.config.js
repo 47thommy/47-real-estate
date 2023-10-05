7d3a9df3b6f779c96910bed71d8f13245025cd13
47thommy <47thommy@gmail.com>
2023-10-05 17:06:02 +0300

    deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,15 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://four7-real-estate.onrender.com",
-
-        secure: false,
-      },
-    },
-  },
-
   plugins: [react()],
 });
